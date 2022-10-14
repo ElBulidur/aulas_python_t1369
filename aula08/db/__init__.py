20aef8cf09a5a385f12e3da7f5aa8ad4e8426683
Julio Pereira <juliocezar.marketing@gmail.com>
2022-10-14 00:01:04 -0300

    explicacao de API e Testes

--- aula08/db/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-# variavel = cria, pega, atualiza, deleta
-
-# arquivos = cria, pega, atualiza, deleta
-
-# banco de dados = cria, pegar, atualiza, deleta (CRUD - CREATE, READ, UPDATE, DELETE)
-
-# API = mandar, pega, atualiza, deleta (REST) [METODOS]
